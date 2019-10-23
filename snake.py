6f55be433e3a1336570770dbec90eb6f22eb39db
Renaud <rendoc007@gmail.com>
2019-10-22 23:38:25 -0400

    initial commit, basic game

---
+++ snake.py
@@ -0,0 +1,63 @@
+import pygame
+
+#CONSTANT #TODO put in config
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
+BODY_WIDTH = 15
+BODY_HEIGHT = 15
+SPACE_BETWEEN_PARTS = 1
+
+GRID_SIZE = 600
+
+class Body(pygame.sprite.Sprite):
+    def __init__(self, pos_x, pos_y, color=WHITE):
+        super().__init__()
+
+        self.image = pygame.Surface([BODY_WIDTH, BODY_HEIGHT])
+        self.image.fill(color)
+
+        self.rect = self.image.get_rect()
+        self.rect.x = pos_x
+        self.rect.y = pos_y
+
+    def __str__(self):
+        return f"({self.rect.x}, {self.rect.y})"
+
+
+class Snake:
+    initial_pos_x = 500
+    initial_pos_y = 60
+
+    def __init__(self, sprite_list, initial_size, alive = True):
+        self.body_parts = []
+        self.sprite_list = sprite_list
+        self.alive = alive
+        self.initial_size = initial_size
+
+        self._build_initial_snake()
+    
+    def _build_initial_snake(self):
+        for i in range(self.initial_size):
+            x = self.initial_pos_x - (BODY_WIDTH + SPACE_BETWEEN_PARTS) * i
+            y = self.initial_pos_y
+
+            body = Body(x, y)
+            self.body_parts.append(body)
+            self.sprite_list.add(body)
+
+
+    def move(self, delta_x, delta_y, cherry):
+        if not cherry:
+            body_tail = self.body_parts.pop()
+            self.sprite_list.remove(body_tail)
+
+        head_pos_x = self.body_parts[0].rect.x + delta_x
+        head_pos_y = self.body_parts[0].rect.y + delta_y
+
+        head = Body(head_pos_x, head_pos_y)
+        self.body_parts.insert(0, head)
+        self.sprite_list.add(head)
+
+        # print(self.body_parts[0])
+
---
+++ snake_game.py
@@ -0,0 +1,106 @@
+import pygame
+from random import randrange
+
+from snake import Snake, Body
+
+#CONSTANT
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (220,20,60)
+
+BODY_WIDTH = 15
+BODY_HEIGHT = 15
+SPACE_BETWEEN_PARTS = 1
+
+GRID_SIZE = 600
+
+def main():
+
+    pygame.init()
+
+    # TODO ADD GRID OBJECT WITH SNAKE AND CHERRY AND SCREEN AREA
+    grid = pygame.display.set_mode([GRID_SIZE, GRID_SIZE])
+    pygame.display.set_caption("ML learn snake")
+
+    sprite_list = pygame.sprite.Group()
+
+    clock = pygame.time.Clock()
+    
+    snake = Snake(sprite_list, initial_size=2)
+
+    delta_x = BODY_WIDTH + SPACE_BETWEEN_PARTS
+    delta_y = 0
+
+    #initial cherry
+    cherry = Body((randrange(1, GRID_SIZE/BODY_WIDTH, 1)*BODY_WIDTH), (randrange(1, GRID_SIZE/BODY_WIDTH, 1)*BODY_WIDTH), RED)
+    snake.sprite_list.add(cherry)
+
+    while snake.alive:
+        cherry_eaten = False
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                # you killed the snake by quitting
+                snake.alive = False
+
+            if event.type == pygame.KEYDOWN:
+                # initial speed
+                # print(event.key)
+                if event.key == pygame.K_a:
+                    delta_x = (BODY_WIDTH + SPACE_BETWEEN_PARTS) * -1
+                    delta_y = 0
+                if event.key == pygame.K_d:
+                    delta_x = (BODY_WIDTH + SPACE_BETWEEN_PARTS) * 1
+                    delta_y = 0
+                if event.key == pygame.K_w:
+                    delta_x = 0
+                    delta_y = (BODY_WIDTH + SPACE_BETWEEN_PARTS) * -1
+                if event.key == pygame.K_s:
+                    delta_x = 0
+                    delta_y = (BODY_WIDTH + SPACE_BETWEEN_PARTS) * 1
+
+        if snake.body_parts[0].rect.colliderect(cherry):
+            snake.sprite_list.remove(cherry)
+            cherry_eaten = True
+
+        snake.move(delta_x, delta_y, cherry_eaten)    
+
+        # check colisions
+        snake.alive = not(is_colisions(snake))
+
+        # spawn cherry
+        # TODO ONLY SPAWN CHERRY EXACTLY ON THE GRID
+        if cherry_eaten:
+            cherry = Body((randrange(1, GRID_SIZE/BODY_WIDTH, 1)*BODY_WIDTH), (randrange(1, GRID_SIZE/BODY_WIDTH, 1)*BODY_WIDTH), RED)
+            snake.sprite_list.add(cherry)
+        # clean screen:
+        grid.fill(BLACK)
+
+        #draw
+        snake.sprite_list.draw(grid)
+
+        # turn on screen
+        pygame.display.flip()
+
+        clock.tick(10)
+    print("END")
+    pygame.quit()
+
+def is_colisions(snake: Snake):
+    # fix colisions with wall
+    def _colisions_with_wall():
+        return snake.body_parts[0].rect.x <= 0 or snake.body_parts[0].rect.x >= GRID_SIZE-BODY_WIDTH or snake.body_parts[0].rect.y <= 0 or snake.body_parts[0].rect.y >= GRID_SIZE-BODY_WIDTH
+    
+    def _colisions_with_itself():
+        head = snake.body_parts[0]
+        # zero is head
+        for parts in snake.body_parts[1:]:
+            if parts.rect.colliderect(head):
+                return True
+        return False
+    # print(_colisions_with_itself() or _colisions_with_wall())
+    return _colisions_with_itself() or _colisions_with_wall()
+    
+    
+if __name__ == "__main__":
+    main()
